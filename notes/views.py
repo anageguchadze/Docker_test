48c5f734a2a867a6fe95adb6ee2d1ec79cb6ae36
Ana Geguchadze <anageguchadze22@gmail.com>
2024-12-08 13:27:05 +0100

    Initial Project Setup

---
+++ notes/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+
+
+class NoteList(generics.ListAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
